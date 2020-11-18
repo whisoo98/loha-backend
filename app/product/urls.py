2634da94039719fc28179369005397da7644d008
mekind <mekind98@gmail.com>
2020-11-18 16:39:48 +0900

    김동성/Product+detail/api불러오기

--- app/product/urls.py
+++ app/product/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('product/', include('product.urls')),
+    path('/', list_all_products()),
+    path('/<string:product_no>/', get_a_product())
 ]
--- app/product/views.py
+++ app/product/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views import View
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+import requests
+
+from rest_framework import request
+from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def list_all_products():
+    url = "https://mekind.cafe24api.com/api/v2/products"
+
+    headers = {
+        'Content-Type': "application/json",
+        'X-Cafe24-Api-Version': "1",
+        'X-Cafe24-Client-Id': "ehSKOHqFTiAKp4coWMTCaH"
+    }
+    response = requests.request("GET", url, headers=headers)
+    return JsonResponse(response)
+
+def get_a_product(self, request, product_no):
+    url = "https://mekind.cafe24api.com/api/v2/products/"+product_no
+
+    headers = {
+        'Content-Type': "application/json",
+        'X-Cafe24-Api-Version': "1",
+        'X-Cafe24-Client-Id': "ehSKOHqFTiAKp4coWMTCaH"
+    }
+    response = requests.request("GET", url, headers=headers)
+    return JsonResponse(response)
