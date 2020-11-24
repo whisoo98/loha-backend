c8d0ad3d471aa0fd9824526bfbb37377fc23859a
김동성 <52501686+mekind@users.noreply.github.com>
2020-11-24 17:54:39 +0900

    Update views.py

--- app/product/views.py
+++ app/product/views.py
@@ -6,7 +6,7 @@ import requests
 
 from rest_framework import request
 from rest_framework.response import Response
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 
@@ -16,19 +16,21 @@ def list_all_products():
 
     headers = {
         'Content-Type': "application/json",
-        'X-Cafe24-Api-Version': "1",
+        'X-Cafe24-Api-Version': "",
         'X-Cafe24-Client-Id': "ehSKOHqFTiAKp4coWMTCaH"
     }
     response = requests.request("GET", url, headers=headers)
-    return JsonResponse(response)
+    #return JsonResponse(response)
+    return HttpResponse(response)
 
 def get_a_product(self, request, product_no):
     url = "https://mekind.cafe24api.com/api/v2/products/"+product_no
 
     headers = {
         'Content-Type': "application/json",
-        'X-Cafe24-Api-Version': "1",
+        'X-Cafe24-Api-Version': "",
         'X-Cafe24-Client-Id': "ehSKOHqFTiAKp4coWMTCaH"
     }
     response = requests.request("GET", url, headers=headers)
-    return JsonResponse(response)
+    #return JsonResponse(response)
+    return HttpResponse(response)
