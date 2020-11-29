4584a382a83023a2970121cde6568c579ec944e7
whisoo98 <whisoo98@gmail.com>
2020-11-29 13:24:36 +0900

    김휘수/Prod/model 및 view

--- app/category/models.py
+++ app/category/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Categories(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, blank=True, default='')
-    cafe24_user_id = models.CharField(max_length=100, blank=True, default='')
+    #cafe24_user_id = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
         ordering = ['created']
--- app/category/views.py
+++ app/category/views.py
@@ -8,18 +8,17 @@ from rest_framework import generics
 from rest_framework import mixins
 import json
 import requests
+import requests
 
-def list_all_categories(request):
-
+def list_all_categories():
     url = "https://mekind.cafe24api.com/api/v2/categories"
 
     headers = {
         'Content-Type': "application/json",
         'X-Cafe24-Api-Version': "",
         'X-Cafe24-Client-Id': "ehSKOHqFTiAKp4coWMTCaH"
-    }
+        }
 
     response = requests.request("GET", url, headers=headers)
-
     return JsonResponse(response.json())
 
