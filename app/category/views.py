3dffadd5eaa837b075b49515b61034c3777ac866
whisoo98 <whisoo98@gmail.com>
2020-12-07 21:26:48 +0900

    김휘수/CategoryList/collection_list FBV

--- app/category/views.py
+++ app/category/views.py
@@ -6,12 +6,15 @@ from django.http import JsonResponse, HttpResponse,Http404
 
 from rest_framework import generics
 from rest_framework import mixins
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,parser_classes
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 import json
 import requests
 from clayful import Clayful
 
 @api_view(['GET'])
+@parser_classes((JSONParser,))
 def collection_list(reqeust):
     Clayful.config({
         'client': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Ijc3MGYzMDA2MTlkYjRhMjBiOGYyY2E5MzZlMDU5YzBmMjE4ZTFjNTE2YmI2ZmQzOWQxN2MyZTE0NTIzN2MzMzAiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjAwNjc5ODY3LCJzdG9yZSI6IjQ1VEdYQjhYTEFLSi45NzMzQTRLRDkyWkUiLCJzdWIiOiJSTUM4WldVUTRFWkUifQ.tcG30RcADqDIj73fRbcIi8b2_u3LlhtXWVaL3SawHRs'
@@ -31,7 +34,7 @@ def collection_list(reqeust):
         headers = result.headers
         data = result.data
 
-        return HttpResponse(data)
+        return Response(data)
         #return JsonResponse(result)
 
 
