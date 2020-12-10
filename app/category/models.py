131fdb81f07e82996f83ce838ac1401de7d1d89b
whisoo98 <whisoo98@gmail.com>
2020-12-11 00:44:26 +0900

    김휘수/Category/Model delete

--- app/category/models.py
+++ app/category/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Categories(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=True, default='')
-    #cafe24_user_id = models.CharField(max_length=100, blank=True, default='')
-
-    class Meta:
-        ordering = ['created']
-
