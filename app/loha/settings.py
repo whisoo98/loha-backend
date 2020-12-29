2fe05d1f22f92b4112c6792db3e8911a940d2a6c
mekind <mekind98@gmail.com>
2020-12-29 21:57:15 +0900

    user 검토 완료

--- app/loha/settings.py
+++ app/loha/settings.py
@@ -12,25 +12,23 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-
-#DEBUG = int(os.environ.get("DEBUG", default=0))
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 SECRET_KEY = '=v^h@)$nwmhieg5%s*t_-q1d*7f2d)!0ucmc*(!0l#3wk66(4j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = int(os.environ.get("DEBUG", default=0))
 DEBUG = True
+
+# HOST
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
-#ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -132,3 +130,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 창 닫으면 로그아웃
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
