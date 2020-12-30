c4e4047ee3114b61cdfc249bb1389a55a6f43a67
whisoo98 <whisoo98@gmail.com>
2020-12-30 17:54:02 +0900

    catalog

---
+++ app/catalog/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
+from django.views import View
+from django.http import JsonResponse, HttpResponse,Http404
+from django.conf import settings
+
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.decorators import api_view,parser_classes
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import json
+import requests
+from clayful import Clayful
+
+
+class CatalogAPI(APIView):
+    Clayful.config({
+        'client': getattr(settings, 'CLAYFUL_SECRET_KEY', None),
+        'language': 'ko',
+        'currency': 'KRW',
+        'time_zone': 'Asia/Seoul',
+        'debug_language': 'ko',
+    })
+
+    def post(self, request):
+        try:
+            Catalog = Clayful.Catalog
+            payload = request.data['payload']
+            options = {
+
+            }
+
+            result = Catalog.create(payload, options)
+
+            headers = result.headers
+            data = result.data
+
+            return Response(data)
+
+        except Exception as e:
+
+            return Response(e.code)
+
+    def get(self, request):
+        try:
+
+            Catalog = Clayful.Catalog
+
+            options = {
+                'query': {
+
+                },
+            }
+
+            result = Catalog.list(options)
+
+            headers = result.headers
+            data = result.data
+
+            return Response(data)
+
+        except Exception as e:
+
+            return Response(e.code)
+
+
+class CatalogDetailAPI(APIView):
+    Clayful.config({
+        'client': getattr(settings, 'CLAYFUL_SECRET_KEY', None),
+        'language': 'ko',
+        'currency': 'KRW',
+        'time_zone': 'Asia/Seoul',
+        'debug_language': 'ko',
+    })
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def delete(self, request):
+        pass
