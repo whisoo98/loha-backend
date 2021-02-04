64711478ef84d5a3b6ea7e8cc2729c27074aaf6e
mekind <mekind98@gmail.com>
2021-02-04 14:52:53 +0900

    서버 실행 가능

--- app/loha/settings.py
+++ app/loha/settings.py
@@ -185,7 +185,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.abspath('.\\static')
+STATIC_ROOT = os.path.abspath('./static')
 STATIC_URL = '/static/'
 
 
