27add37330f7ecb5e3ba57c5a451dfa9cdb37aba
mekind <mekind98@gmail.com>
2021-02-15 19:26:41 +0900

    redis setting

--- app/loha/settings.py
+++ app/loha/settings.py
@@ -128,7 +128,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(REDIS_HOST, 6379)],
+            "hosts": [('redis', 6379)],
         },
     },
 }
