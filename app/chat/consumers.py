19206a22c31e7d12cc7621491e2611c67baed8ef
mekind <mekind98@gmail.com>
2021-02-15 13:53:51 +0900

    로그인 안 하면 연결 x

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
+from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
 import json
 import pprint
@@ -12,6 +13,8 @@ from channels.db import database_sync_to_async
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.username = self.scope['user'].get_username()
+        if self.username == "":
+            raise StopConsumer
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
