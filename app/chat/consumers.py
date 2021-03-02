f28919f668ea8e12b0461cd1e7a782a96ae72d22
mekind <mekind98@gmail.com>
2021-03-02 14:49:37 +0900

    여러방 가능

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -15,11 +15,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        try:
-            await self.check_room()
-            await self.accept()
-        except:
-            await self.close()
+        await self.accept()
+        # try:
+        #     await self.check_room()
+        #     await self.accept()
+        # except:
+        #     await self.close()
 
 
     # FOR GOING OUT
