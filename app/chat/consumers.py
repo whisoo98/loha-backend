7bd6c80d7851a1918e2d5ee14a7cbe85d7e56d80
mekind <mekind98@gmail.com>
2021-03-08 15:35:39 +0900

    최종완료

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -15,7 +15,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        print("connect")
+
         # check room exist
         try:
             now_room = await self.check_room()
