0ff31013bdce807c540bf6d0d138a7384747fa33
sjwhole <bluemoon2718@naver.com>
2021-04-28 02:21:50 +0900

    방송이 종료 시 stat: end 보내기 - 그룹으로 보냄

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -80,6 +80,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'message': message
                 }
             )
+        elif text_data_json['stat'] == 'end':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'live_end',
+                    'username': self.username,
+                    'message': "방송이 종료되었습니다."
+                }
+            )
         else:
             message = text_data_json['message']
 
@@ -120,6 +129,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'stat': 'chat',
             'message': message,
             'username': username
+        }))
+
+    async def live_end(self, event):
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'stat': 'end',
+            'message': "방송이 종료되었습니다.",
+            'username': "Byeolshow",
         }))
 
     @database_sync_to_async
