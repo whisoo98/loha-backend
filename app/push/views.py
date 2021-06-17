2390b7f36cb36427b40c00001e8dcad9dd0a8620
sjwhole <bluemoon2718@naver.com>
2021-06-17 19:06:19 +0900

    feat: Add alarm delay 30 seconds

--- app/push/views.py
+++ app/push/views.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from firebase_admin import messaging
 from firebase_admin.exceptions import FirebaseError
@@ -90,6 +91,7 @@ async def alarm_by_user_id(user_ids, info):
     futures = [
         asyncio.ensure_future(send_message(token, info)) for token in set(registration_tokens)
     ]
+    time.sleep(30)
     await asyncio.gather(*futures)
 
 
