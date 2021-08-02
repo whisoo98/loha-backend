cde36329e9eba2210640369477b923cb2c4681a7
sjwhole <bluemoon2718@naver.com>
2021-08-02 17:12:14 +0900

    feat: Add thumbnail to media serializer

--- app/media/serializers.py
+++ app/media/serializers.py
@@ -4,9 +4,23 @@ from .models import *
 
 
 class MediaSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.SerializerMethodField()
+
     class Meta:
         model = MediaStream
         fields = '__all__'
+
+    def get_thumbnail(self, media):
+        asset_playback_id = media.mux_asset_playback_id
+        live_playback_id = media.mux_livestream_playback_id
+
+        if asset_playback_id:
+            playback_id = asset_playback_id
+        else:
+            playback_id = live_playback_id
+
+        url = f"https://image.mux.com/{playback_id}/thumbnail.png"
+        return url
 
 
 class MediaSerializerforClient(serializers.ModelSerializer):
